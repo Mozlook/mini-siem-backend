21a998703f0f4c609c31281bc82f12af32c01992
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-03-03 19:06:47 +0100

    appState schema

---
+++ schemas/appState.py
@@ -0,0 +1,10 @@
+from threading import Thread
+from typing import Protocol
+from schemas.ingest import IngestState
+from _thread import LockType
+
+
+class ReadyAppState(Protocol):
+    ingest_thread: Thread | None
+    ingest_lock: LockType | None
+    ingest_state: IngestState | None
